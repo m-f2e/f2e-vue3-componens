72291563c57278e8771f3c0f61453a7d0ec8e38f
zhouwei <zhouwei1@cheyipai.com>
2023-06-29 19:11:03 +0800

    update

--- site/.vuepress/config.ts
+++ site/.vuepress/config.ts
@@ -1,7 +1,7 @@
 import { defineUserConfig, defaultTheme, viteBundler } from 'vuepress'
 import registerComponentsPlugin from '@vuepress/plugin-register-components'
 import { codeBlockPlugin } from '@yanyu-fe/vuepress-plugin-code-block'
-import { docsearchPlugin } from '@vuepress/plugin-docsearch'
+import { searchPlugin } from '@vuepress/plugin-search'
 import { head, navEn, navZh, sideEn, sideZh } from './configs'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import { resolve } from 'path'
@@ -14,57 +14,6 @@ export default defineUserConfig({
   head,
 	plugins: [
     codeBlockPlugin(),
-    docsearchPlugin({
-      appId: '34YFD9IUQ2',
-      apiKey: '9a9058b8655746634e01071411c366b8',
-      indexName: 'vuepress',
-      searchParameters: {
-        facetFilters: ['tags:v2'],
-      },
-      locales: {
-        '/zh/': {
-          placeholder: '搜索文档',
-          translations: {
-            button: {
-              buttonText: '搜索文档',
-              buttonAriaLabel: '搜索文档',
-            },
-            modal: {
-              searchBox: {
-                resetButtonTitle: '清除查询条件',
-                resetButtonAriaLabel: '清除查询条件',
-                cancelButtonText: '取消',
-                cancelButtonAriaLabel: '取消',
-              },
-              startScreen: {
-                recentSearchesTitle: '搜索历史',
-                noRecentSearchesText: '没有搜索历史',
-                saveRecentSearchButtonTitle: '保存至搜索历史',
-                removeRecentSearchButtonTitle: '从搜索历史中移除',
-                favoriteSearchesTitle: '收藏',
-                removeFavoriteSearchButtonTitle: '从收藏中移除',
-              },
-              errorScreen: {
-                titleText: '无法获取结果',
-                helpText: '你可能需要检查你的网络连接',
-              },
-              footer: {
-                selectText: '选择',
-                navigateText: '切换',
-                closeText: '关闭',
-                searchByText: '搜索提供者',
-              },
-              noResultsScreen: {
-                noResultsText: '无法找到相关结果',
-                suggestedQueryText: '你可以尝试查询',
-                reportMissingResultsText: '你认为该查询应该有结果？',
-                reportMissingResultsLinkText: '点击反馈',
-              },
-            },
-          },
-        },
-      },
-    }),
     registerComponentsPlugin({
       componentsDir: resolve(__dirname, './components'),
     }),
